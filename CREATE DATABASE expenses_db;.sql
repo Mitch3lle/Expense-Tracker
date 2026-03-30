6d73e4dc1cd605ca0e7c2740a71575efd9a3411b
Mitch3lle <wakwayamitchelle@gmail.com>
2026-03-30 17:02:10 +0300

    Update db

--- CREATE DATABASE expenses_db;.sql
+++ CREATE DATABASE expenses_db;.sql
@@ -1,11 +1,19 @@
 CREATE DATABASE expenses_db;
-
 USE expenses_db;
-
 CREATE TABLE expenses (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  amount DECIMAL(10,2),
+  amount DECIMAL(10, 2),
   category VARCHAR(50),
   date DATE,
   description TEXT
 );
+USE expenses_db;
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(50),
+  password VARCHAR(100)
+);
+ALTER TABLE expenses
+ADD COLUMN user_id INT;
+INSERT INTO expenses_db { user_id }
+VALUES { 'John Doe' }
